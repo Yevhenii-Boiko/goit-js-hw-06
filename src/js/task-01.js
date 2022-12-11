2b3cd11d981db28f087bd5e5c266c6f49c560c85
Yevhenii <boiko.yevgenii@gmail.com>
2022-12-11 14:18:05 +0200

    adds task-1 solution

--- src/js/task-01.js
+++ src/js/task-01.js
@@ -2,4 +2,8 @@ const numberOfCategories = document.querySelectorAll('.item');
 console.log(`Number of categories: ${numberOfCategories.length}`);
 
 const listOfItems = document.querySelectorAll('h2');
-console.log(listOfItems);
+listOfItems.forEach(function (item) {
+  console.log(
+    `Category: ${item.textContent}; elements: ${item.nextElementSibling.childElementCount}`,
+  );
+});
--- src/js/task-02.js
+++ src/js/task-02.js
@@ -7,5 +7,8 @@ const ingredients = [
   'Condiments',
 ];
 
+const list = document.querySelector('#ingredients');
 const itemEl = document.createElement('li');
+itemEl.textContent = '';
+itemEl.classList.toggle('item');
 console.log(itemEl);
