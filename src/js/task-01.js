5ca7b956ec46bc9a6a855c59de8fc26782b86d38
Yevhenii <boiko.yevgenii@gmail.com>
2022-12-12 23:29:57 +0200

    Update task-01.js

--- src/js/task-01.js
+++ src/js/task-01.js
@@ -1,9 +1,11 @@
-const numberOfCategories = document.querySelectorAll('.item');
-console.log(`Number of categories: ${numberOfCategories.length}`);
+const categoriesById = document.querySelector('#categories');
+const categoriesList = categoriesById.querySelectorAll('.item');
+const numberOfCategories = categoriesList.length;
+console.log(`Number of categories: ${numberOfCategories}`);
 
-const listOfItems = document.querySelectorAll('h2');
-listOfItems.forEach(function (item) {
-  console.log(
-    `Category: ${item.textContent}; elements: ${item.nextElementSibling.childElementCount}`,
-  );
+categoriesList.forEach(element => {
+  const categoryName = element.querySelector('h2').textContent;
+  const numberOfCategories = element.querySelector('ul').children.length;
+
+  console.log(`Category: ${categoryName}; elements: ${numberOfCategories}`);
 });
