3aefb916b440f80fde3d381987d1cbd238e5db07
Yevhenii <boiko.yevgenii@gmail.com>
2022-12-24 21:28:20 +0200

    adds task-09 solution

--- src/js/task-09.js
+++ src/js/task-09.js
@@ -1,3 +1,14 @@
+const button = document.querySelector('.change-color');
+const color = document.querySelector('.color');
+console.log(button);
+console.log(color);
+button.addEventListener('click', () => {
+  color.textContent = getRandomHexColor();
+  document.body.style.backgroundColor = color.textContent;
+});
+
 function getRandomHexColor() {
-  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
 }
