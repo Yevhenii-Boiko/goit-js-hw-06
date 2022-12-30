229178183f11995c161046e4f54cde8f44ab0dad
Yevhenii <boiko.yevgenii@gmail.com>
2022-12-30 17:09:30 +0200

    Update task-08.js

--- js/task-08.js
+++ js/task-08.js
@@ -1,6 +1,6 @@
-const form = document.querySelector('.login-form');
+const form = document.querySelector(".login-form");
 
-form.addEventListener('submit', handleSubmit);
+form.addEventListener("submit", handleSubmit);
 
 function handleSubmit(event) {
   event.preventDefault();
@@ -8,8 +8,8 @@ function handleSubmit(event) {
     elements: { email, password },
   } = event.currentTarget;
 
-  if (email.value === '' || password.value === '') {
-    alert('Please fill in all the fields!');
+  if (email.value === "" || password.value === "") {
+    return alert("Please fill in all the fields!");
   }
 
   console.log(`Login: ${email.value}, Password: ${password.value}`);
